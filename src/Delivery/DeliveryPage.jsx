ba36d51c0a2279cb3048a3ec68e486f2a1ebfc85
ngo13082k2 <89001718+ngo13082k2@users.noreply.github.com>
2025-04-01 13:12:43 +0700

    deliveryPage

--- src/Delivery/DeliveryPage.jsx
+++ src/Delivery/DeliveryPage.jsx
@@ -182,7 +182,7 @@ const DeliveryPage = () => {
                     <input type="file" accept=".xlsx" onChange={handleFileChange} className="mb-2" />
                     <button
                         onClick={handleUpload}
-                        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 flex items-center justify-center"
+                        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
                         disabled={isUploading}>
                         {isUploading ? <ClipLoader size={20} color="#ffffff" /> : "Upload"}
                     </button>                    <button
